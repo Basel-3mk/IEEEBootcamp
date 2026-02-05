8fc2115c68e2960500b19668486b04a3dc1f1b41
Basel Al-Jabari <baslimadj@gmail.com>
2026-02-05 18:12:32 +0200

    Implementing CRUD Operations

---
+++ Training/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Training.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public static List<string> namesList = new List<string> { "Basel", "Belal", "Haitham" };
+
+        [HttpGet]
+        public static List<string> GetNames()
+        {
+            return namesList;
+        }
+
+        [HttpPost]
+        public static List<string> AddName(string newName)
+        {
+            namesList.Add(newName);
+            return namesList;
+        }
+
+        [HttpPut]
+        public static List<string> UpdateName(int oldIndex, string newName)
+        {
+            namesList[oldIndex] = newName;
+            return namesList;
+        }
+
+        [HttpDelete]
+        public static List<string> DeleteName(int index)
+        {
+            namesList.RemoveAt(index);
+            return namesList;
+        }
+    }
+}
