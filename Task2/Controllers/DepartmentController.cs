18532a162a06c889cecce8821aea71b57e53e3b1
Basel Al-Jabari <baslimadj@gmail.com>
2026-02-09 20:55:19 +0200

    Implementing Controllers

---
+++ Task2/Controllers/DepartmentController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task2.Database;
+using Task2.Models;
+
+namespace Task2.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DepartmentController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    DepartmentController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetDepartments()
+    {
+        var departments = _context.departments.ToList();
+        return Ok(departments);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetDepartmentById([FromRoute] Guid id)
+    {
+        var department = _context.departments.Find(id);
+
+        if (department == null)
+        {
+            return NotFound();
+        }
+
+        else
+        {
+            return Ok(department);
+        }
+    }
+
+    [HttpPost]
+    public IActionResult AddDepartment([FromBody] string newName)
+    {
+        if (newName.Length > 20)
+        {
+            return BadRequest("The Name Is Too Long");
+        }
+
+        else
+        {
+            var department = new Department
+            {
+                Name = newName
+            };
+
+            _context.departments.Add(department);
+            _context.SaveChanges();
+            return Created();
+        }
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateDepartment([FromRoute] Guid oldId, [FromBody] string newName)
+    {
+        var department = _context.departments.Find(oldId);
+
+        if (department == null)
+        {
+            return NotFound();
+        }
+
+        else if (newName.Length > 20)
+        {
+            return BadRequest("The Name Is Too Long");
+        }
+
+        else
+        {
+            department.Name = newName;
+            _context.SaveChanges();
+            return Created();
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteDepartment([FromRoute] Guid id)
+    {
+        var department = _context.departments.Find(id);
+
+        if (department == null)
+        {
+            return NotFound();
+        }
+
+        else
+        {
+            _context.departments.Remove(department);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ Task2/Controllers/EmployeeController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Task2.Database;
+using Task2.DTO;
+using Task2.Models;
+
+namespace Task2.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeeController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    EmployeeController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetEmployees()
+    {
+        var employees = _context.employees.Include(curEmployee => curEmployee.DepartmentId).Select(curEmployee => new EmployeeResponseDTO
+        {
+            Id = curEmployee.Id,
+            Name = curEmployee.Name,
+            DepartmentId = curEmployee.DepartmentId,
+            DepartmentName = curEmployee.Department.Name
+        }).ToList();
+
+        return Ok(employees);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetEmployeeById([FromRoute] Guid id)
+    {
+        var employee = _context.employees.Include(curEmployee => curEmployee.DepartmentId).Select(curEmployee => new EmployeeResponseDTO
+        {
+            Id = curEmployee.Id,
+            Name = curEmployee.Name,
+            DepartmentId = curEmployee.DepartmentId,
+            DepartmentName = curEmployee.Department.Name
+        }).First();
+
+        if (employee == null)
+        {
+            return NotFound();
+        }
+
+        else
+        {
+            return Ok(employee);
+        }
+    }
+
+    [HttpPost]
+    public IActionResult AddEmployee([FromBody] EmployeeRequestDTO employeeDTO)
+    {
+        Employee newEmployee = new Employee
+        {
+            Id = employeeDTO.Id,
+            Name = employeeDTO.Name,
+            DepartmentId = employeeDTO.DepartmentId
+        };
+
+        _context.employees.Add(newEmployee);
+        _context.SaveChanges();
+        return Created();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateEmployee([FromRoute] Guid id, [FromBody] EmployeeRequestDTO employeeDTO)
+    {
+        var oldEmployee = _context.employees.Find(id);
+
+        Employee newEmployee = new Employee
+        {
+            Id = employeeDTO.Id,
+            Name = employeeDTO.Name,
+            DepartmentId = employeeDTO.DepartmentId
+        };
+
+        if (oldEmployee == null)
+        {
+            return NotFound();
+        }
+
+        else if (newEmployee.Name.Length > 20)
+        {
+            return BadRequest("Name Is Too Long");
+        }
+
+        else
+        {
+            oldEmployee = newEmployee;
+            _context.SaveChanges();
+            return Created();
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteEmployee([FromRoute] Guid id)
+    {
+        var employee = _context.employees.Find(id);
+
+        if (employee == null)
+        {
+            return NotFound();
+        }
+
+        else
+        {
+            _context.employees.Remove(employee);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
