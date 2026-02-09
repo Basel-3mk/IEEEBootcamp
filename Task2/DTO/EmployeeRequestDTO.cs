21c1a6fa0261b20177c4cbc94ae258129afa8a16
Basel Al-Jabari <baslimadj@gmail.com>
2026-02-09 20:34:51 +0200

    Creating DTOs

---
+++ Task2/DTO/EmployeeRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Task2.DTO;
+
+public class EmployeeRequestDTO
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Name { get; set; }
+
+    public Guid DepartmentId { get; set; }
+}
---
+++ Task2/DTO/EmployeeResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Task2.DTO;
+
+public class EmployeeResponseDTO
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Name { get; set; }
+
+    public Guid DepartmentId { get; set; }
+
+    public string DepartmentName { get; set; }
+}
